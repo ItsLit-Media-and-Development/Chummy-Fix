7726f2897befa43c684deb26d4cb8a67a475fccc
Marc Towler <marc@marctowler.co.uk>
2020-06-26 23:14:22 +0100

    Added Config Added basic modification of odds for item 812

---
+++ Chummy Fix/ModConfig.cs
@@ -0,0 +1,52 @@
+﻿namespace ChummyFix
+{
+    internal class ModConfig
+    {
+        public int Roe { get; set; } = 50;
+        public int Amethyst { get; set; } = 50;
+        public int Diamond { get; set; } = 50;
+        public int Quartz { get; set; } = 50;
+        public int Frozen_Tear { get; set; } = 50;
+        public int Seaweed { get; set; } = 50;
+        public int Green_Algae { get; set; } = 50;
+        public int White_Algae { get; set; } = 50; 
+        public int Treasure_Chest { get; set; } = 50;
+        public int Trash { get; set; } = 50;
+        public int Driftwood { get; set; } = 50;
+        public int Broken_Glasses { get; set; } = 50;
+        public int Broken_CD { get; set; } = 50;
+        public int Soggy_Newspaper { get; set; } = 50;
+        public int Spicy_Eeel { get; set; } = 50;
+        public int Void_Egg { get; set; } = 50;
+        public int Acorn { get; set; } = 50;
+        public int Maple_Seed { get; set; } = 50;
+        public int Pine_Cone { get; set; } = 50;
+        public int Refined_Quartz { get; set; } = 50;
+        public int Copper_Ore { get; set; } = 50;
+        public int Iron_Ore { get; set; } = 50;
+        public int Gold_Ore { get; set; } = 50;
+        public int Iridium_Ore { get; set; } = 50;
+        public int Wood { get; set; } = 50;
+        public int Stone { get; set; } = 50;
+        public int Nautilus_Shell { get; set; } = 50;
+        public int Coral { get; set; } = 50;
+        public int Rainbow_Shell { get; set; } = 50;
+        public int Sea_Urchin { get; set; } = 50;
+        public int Pale_Broth { get; set; } = 50;
+        public int Geode { get; set; } = 50;
+        public int Frozen_Geode { get; set; } = 50;
+        public int Magma_Geode { get; set; } = 50;
+        public int Dolomite { get; set; } = 50;
+        public int Limestone { get; set; } = 50;
+        public int Rain_Totem { get; set; } = 50;
+        public int Bug_Meat { get; set; } = 50;
+        public int Farm_Totem { get; set; } = 50;
+        public int Mountains_Totem { get; set; } = 50;
+        public int Beach_Totem { get; set; } = 50;
+        public int Hardwood { get; set; } = 50;
+        public int Omni_Geode { get; set; } = 50;
+        public int Slime { get; set; } = 50;
+        public int Solar_Essence { get; set; } = 50;
+        public int Void_Essence { get; set; } = 50;
+    }
+}
--- Chummy Fix/ModEntry.cs
+++ Chummy Fix/ModEntry.cs
@@ -1,12 +1,85 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Buildings;
+using StardewValley.GameData.FishPond;
 
 namespace ChummyFix
 {
-    class ModEntry
+    class ModEntry : Mod
     {
+        private ModConfig Config;
+        private FishPondReward reward;
+
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            Config = helper.ReadConfig<ModConfig>();
+
+            helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+            helper.Events.GameLoop.Saving     += OnSaving;
+        }
+
+        private void OnSaving(object sender, SavingEventArgs e)
+        {
+            FixOdds();
+        }
+
+        private void OnSaveLoaded(object sender, SaveLoadedEventArgs e)
+        {
+            FixOdds();
+        }
+
+        private void FixOdds()
+        {
+            foreach(var building in Game1.getFarm().buildings)
+            {
+                //are we dealing with ponds?
+                if(building is FishPond pond)
+                {
+                    int currentPopulation = pond.FishCount;
+
+                    //incase pond is empty on load
+                    if(currentPopulation == 0)
+                    {
+                        break;
+                    }
+
+                    //what items can be produced in the pond
+                    foreach(var item in pond.GetFishPondData().ProducedItems)
+                    {
+                        PrintToLog($"Fish: {pond.fishType}, reward ID: {item.ItemID}, chance: {item.Chance}, reqPop: {item.RequiredPopulation}, min: {item.MinQuantity}");
+
+                        if(currentPopulation >= item.RequiredPopulation)
+                        {
+                            //time to fix the odds
+                            switch(item.ItemID)
+                            {
+                                case 812:
+                                    if (item.Chance != (Config.Roe / 100))
+                                    {
+                                        PrintToLog($"Setting Roe's odds to {Config.Roe}%");
+                                        item.Chance = Config.Roe / 100;
+                                        pond.GetFishPondData().SpawnTime = 0;
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void PrintToLog(string message)
+        {
+            this.Monitor.Log(message, LogLevel.Debug);
+        }
     }
 }
